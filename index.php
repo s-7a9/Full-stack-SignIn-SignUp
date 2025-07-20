1132a40553176b38ee6e135747778975c45372d5
s-7a9 <ishakghofrane66@gmail.com>
2025-07-20 02:12:33 +0100

    terminer le backend

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
+<?php
+
+session_start();
+
+$errors = [
+    'login'=>$_SESSION['login_error'] ?? '',
+    'register'=>$_SESSION['register_error'] ?? ''
+];
+$activeForm = $_SESSION['active_form'] ?? 'login';
+
+session_unset();
+
+function showError($error){
+    return !empty($error) ? "<p class='error-message'>$error</p>" : '';
+}
+
+function isActiveForm($formName, $activeForm){
+    return $formName === $activeForm ? 'active' : '';
+}
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,9 +33,10 @@
 <body>
     
      <div class="container">
-        <div class="form-box active" id="login-form">
-            <form action="">
+        <div class="form-box <?= isActiveForm('login', $activeForm); ?>" id="login-form">
+            <form action="login_register.php" method="post">
                 <h2>Login</h2>
+                <?= showError($errors['login']); ?>
                 <input type="email" name="email" placeholder="Email" required>
                 <input type="password" name="password" placeholder="Password" required>
                 <button type="submit" name="login">Login</button>
@@ -19,9 +44,10 @@
             </form> 
         </div>
 
-        <div class="form-box" id="register-form">
-            <form action="">
+        <div class="form-box <?= isActiveForm('register', $activeForm); ?>" id="register-form">
+            <form action="login_register.php" method="post">
                 <h2>Register</h2>
+                <?= showError($errors['register']); ?>
                 <input type="text" name="name" placeholder="name" required>
                 <input type="email" name="email" placeholder="Email" required>
                 <input type="password" name="password" placeholder="Password" required>
